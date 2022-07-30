74dc20cc3bb0025ec1204ab832ea687fd98b80df
harishtpj <harishtpj@hotmail.com>
2022-07-30 13:09:48 +0530

    Added Basic Stdout Print

--- kernel.rs
+++ kernel.rs
@@ -23,20 +23,13 @@
 #![no_std]
 #![no_main]
 
+mod ironlib;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    ironlib::stdout::print_something();
 
     loop {}
 }
